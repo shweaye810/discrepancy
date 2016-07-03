747fd41c4d6513f2dac1f8a9ae27856e4e3c61c0
Shwe Aye <shweaye810@gmail.com>
2016-07-03 14:04:36 -0700

    temporary remove printing sub_PN and sub_qty

--- info.cpp
+++ info.cpp
@@ -7,7 +7,9 @@ using namespace std;
 Info::Info(string part_n, string model) :
         m_pn(part_n), m_mdl(model), m_elm(0), m_rtv(0), xtr_qty(0)
 {
+
 }
+
 
 Info::Info(const Info &o) : m_pn(o.m_pn), m_mdl(o.m_mdl),
                             m_elm(o.m_elm), m_rtv(o.m_rtv), xtr_qty(o.xtr_qty),
@@ -20,8 +22,10 @@ ostream& operator<<(ostream &cout, const Info &info)
         cout << info.m_pn << '\t' << info.m_mdl << '\t'
              << info.m_elm << '\t' << info.m_rtv << '\t'
              << info.xtr_qty << '\t' << info.m_sub.size() << '\t';
+/*
         for (auto itr = info.m_sub.begin(); itr != info.m_sub.end(); ++itr) {
                 cout << itr->first << '\t' << itr->second << '\t';
         }
+*/
         return cout;
 }
