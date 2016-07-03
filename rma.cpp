9c34c068ae661d85932f037a69b89af2863c9689
Shwe Aye <shweaye810@gmail.com>
2016-07-02 17:53:19 -0700

    rma class points to infos of each RMA numbers. implement constructor(), add(), operator<<()

--- rma.cpp
+++ rma.cpp
@@ -2,7 +2,30 @@
 
 using namespace std;
 
-RMA::RMA(string s) : str(s)
+RMA::RMA()
 {
+}
+
+void RMA::add()
+{
+        string rma_nm = Tokenizer::get_token();
+        string pn = Tokenizer::get_token();
+        string mdl = Tokenizer::get_token();
+        int qty = stoi(Tokenizer::get_token());
+        string loc = Tokenizer::get_token();
+        Info nf(pn, mdl, qty, loc);
+        m_info[rma_nm].push_back(nf);
+}
+
+ostream& operator<<(ostream &cout, const RMA &rma)
+{
+        for (auto itr = rma.m_info.begin(); itr != rma.m_info.end(); ++itr) {
+                for (auto iitr = itr->second.begin();
+                     iitr != itr->second.end();
+                     ++iitr) {
+                        cout << itr->first << '\t' << *iitr << endl;
+                }
+        }
+        return cout;
 
 }
--- rma.h
+++ rma.h
@@ -1,9 +1,13 @@
+#include <map>
+
 #include "info.h"
+#include "tokenizer.h"
 
 class RMA{
 public:
-        RMA(std::string s);
+        RMA();
+        void add();
+        friend std::ostream& operator<<(std::ostream &, const RMA &);
 private:
-        std::string str;
-        std::vector<Info> info;
+        std::map<std::string, std::vector<Info> > m_info;
 };
