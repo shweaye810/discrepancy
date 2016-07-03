36f5f50458fcee48c27c35ef8c8d3b9c747ebe58
Shwe Aye <shweaye810@gmail.com>
2016-07-03 14:00:27 -0700

    instead of pushing_back rma everytime, change mapped's rma.

--- asp.cpp
+++ asp.cpp
@@ -12,19 +12,25 @@ void ASP::add(string s)
 {
         Tokenizer::set_string(s);
         string asp_nm = Tokenizer::get_token();
-        RMA rma;
-        rma.add();
-        m_row[asp_nm].push_back(rma);
+        if (m_row.find(asp_nm) == m_row.end()) {
+                RMA rma;
+                rma.add();
+                m_row[asp_nm] = rma;
+        } else {
+                RMA &tmp_rma = m_row[asp_nm];
+                tmp_rma.add();
+        }
 }
 
 ostream& operator<<(ostream &cout, const ASP &asp)
 {
         for (auto itr = asp.m_row.begin(); itr != asp.m_row.end(); ++itr) {
-                for (auto iitr = itr->second.begin();
+                /*for (auto iitr = itr->second.begin();
                      iitr != itr->second.end();
                      ++iitr) {
                         cout << itr->first << '\t' << *iitr << endl;
-                }
+                        }*/
+                cout << itr->first << '\t' << itr->second << endl;
         }
         return cout;
 }
