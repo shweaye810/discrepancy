20c29ed400cddc154d4e3b672df5bb5f1c41ce39
Shwe Aye <shweaye810@gmail.com>
2016-07-03 09:10:58 -0700

    attepmt to increment qty instead of pushing the same info into the vector.

--- rma.cpp
+++ rma.cpp
@@ -1,4 +1,5 @@
 #include "rma.h"
+#include <exception>
 
 using namespace std;
 
@@ -22,13 +23,26 @@ void RMA::add()
         int qty = stoi(Tokenizer::get_token());
         string loc = Tokenizer::get_token();
         string tmp(lower(loc));
-        Info nf(pn, mdl);
-        if (tmp == "rtv") {
-                nf.m_rtv = qty;
-        } else if (tmp == "elm") {
-                nf.m_elm = qty;
+        if (!m_info.empty() && m_info.find(rma_nm) != m_info.end()) {
+                auto nf = m_info.at(rma_nm);
+                for (auto itr = nf.begin(); itr != nf.end(); ++itr) {
+                        if (itr->m_pn == pn) {
+                                if (tmp == "rtv") {
+                                        itr->m_rtv += qty;
+                                } else if (tmp == "elm") {
+                                        itr->m_elm += qty;
+                                }
+                        }
+                }
+        } else {
+                Info nf(pn, mdl);
+                if (tmp == "rtv") {
+                        nf.m_rtv = qty;
+                } else if (tmp == "elm") {
+                        nf.m_elm = qty;
+                }
+                m_info[rma_nm].push_back(nf);
         }
-        //m_info[rma_nm].push_back(nf);
 }
 
 ostream& operator<<(ostream &cout, const RMA &rma)
