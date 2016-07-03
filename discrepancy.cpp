7d1330379a3cb54d54e0653c3833e501ee7c58fc
Shwe Aye <shweaye810@gmail.com>
2016-07-02 17:49:48 -0700

    add tester to test all the classes

--- discrepancy.cpp
+++ discrepancy.cpp
@@ -1,28 +1,56 @@
 #include <fstream>
 #include <iostream>
+#include <sstream>
 
 #include "asp.h"
+#include "tokenizer.h"
 
 using namespace std;
 
+void print_error(const string &file_nm){
+        cout << "Error opening " << file_nm << ".\n" << flush;
+}
 int main()
 {
         ofstream fout;
         ifstream fin;
-        string file_nm("test.txt"), tmp;
+        string in_nm("test.txt"), out_nm("output.txt"), tmp, row;
         cout << "Trying to open test.txt.\n";
-        fin.open(file_nm.c_str());
-        /*
-        cout << "Please enter the file name: " << flush ;
-        cin >> file_nm;
-        fin.open(file_nm.str());
+        fin.open(in_nm.c_str());
+        /*        cout << "Please enter the file name: " << flush ;
+                  cin >> in_nm;
+                  fin.open(in_nm.str());
         */
-        if(!fin.is_open()) {
-                cout << "Error opening " << file_nm << ".\n" << flush;
+        if (!fin.is_open()) {
+                print_error(in_nm);
                 return -1;
         }
+
+        fout.open(out_nm.c_str());
+        if (!fout.is_open()) {
+                print_error(out_nm);
+                return -1;
+        }
+        ASP list;
+        int i = 0;
         do {
-                getline(fin, tmp);
+                stringstream ss;
+                getline(fin, row);
+                list.add(row);
+                cout << list;
+
+                /*
+                  ss.str(tmp);
+                  for (int i = 0; getline(ss, tmp, '\t'); ++i) {
+                  fout << "i = " << i << ". " << tmp << " ";
+                  }
+                  fout << endl;
+                */
+                cout << i << endl;
+                ++i;
         } while(fin.good());
+        fin.close();
+        fout.close();
+        cout << "End of program.\n" << flush;
         return 0;
 }
