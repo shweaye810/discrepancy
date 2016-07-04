2ac1eb0df3d50e8d5b57b9f8995dd91bd4e0b328
Shwe Aye <shweaye810@gmail.com>
2016-07-03 19:50:48 -0700

    add print() declaration.

--- rma.h
+++ rma.h
@@ -7,6 +7,7 @@ class RMA{
 public:
         RMA();
         void add();
+        void print(std::ostream &, const std::string &s) const;
         friend std::ostream& operator<<(std::ostream &, const RMA &);
 private:
         std::map<std::string, std::vector<Info> > m_info;
