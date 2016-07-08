5882dcd7fa1dd1eb223ec2be5f0a29f095b2c0ba
Shwe Aye <shweaye810@gmail.com>
2016-07-07 20:36:51 -0700

    add() now uses helper proc_loc(), instead of doing everything in it. implements RMA_qty_calc()

--- rma.cpp
+++ rma.cpp
@@ -14,7 +14,19 @@ string lower(string s)
         }
         return s;
 }
-
+inline void RMA::proc_loc(Info &nf, const string &tmp, int qty)
+{
+        if (tmp == "rtv") {
+                nf.m_rtv += qty;
+        } else if (tmp == "elm") {
+                nf.m_elm += qty;
+        }
+}
+/*
+ *
+ *
+ *
+ */
 void RMA::add()
 {
         string rma_nm = Tokenizer::get_token();
@@ -24,23 +36,30 @@ void RMA::add()
         string loc = Tokenizer::get_token();
         string tmp(lower(loc));
         Info nf(pn, mdl);
+        proc_loc(nf, tmp);
+        /*
+         * replaced by proc_loc
         if (tmp == "rtv") {
                 nf.m_rtv = qty;
         } else if (tmp == "elm") {
                 nf.m_elm = qty;
         }
-
+        */
         if (m_info.find(rma_nm) == m_info.end()) {
                 m_info[rma_nm].push_back(nf);
         } else {
                 auto &vt = m_info.at(rma_nm);
                 for (auto itr = vt.begin(); itr != vt.end(); ++itr) {
                         if (itr->m_pn == pn) {
+                                proc_loc(*itr, tmp, qty);
+                                /*
+                                 * replace by proc_loc()
                                 if (tmp == "rtv") {
                                         itr->m_rtv += qty;
                                 } else if (tmp == "elm") {
                                         itr->m_elm += qty;
                                 }
+                                */
                                 return;
                         }
                 }
@@ -69,6 +88,24 @@ ostream& operator<<(ostream &cout, const RMA &rma)
                      iitr != itr->second.end();
                      ++iitr) {
                         cout << itr->first << '\t' << *iitr << endl;
+                }
+        }
+        return cout;
+
+}
+
+void RMA::do_PN_substitude()
+{
+
+}
+
+void RMA::RMA_qty_calc()
+{
+        for (auto itr = rma.m_info.begin(); itr != rma.m_info.end(); ++itr) {
+                for (auto iitr = itr->second.begin();
+                     iitr != itr->second.end();
+                     ++iitr) {
+                        iitr->xtr_qty = m_elm - m_rtv + m_sub_tot;
                 }
         }
         return cout;
