712523400fc53fd6c6c0556baaddfb0e2441ec1d
Shwe Aye <shweaye810@gmail.com>
2016-07-07 20:37:30 -0700

    declare RMA_PN_substitude(), RMA_qty_calc(), and proc_loc()

--- rma.h
+++ rma.h
@@ -9,6 +9,9 @@ public:
         void add();
         void print(std::ostream &, const std::string &s) const;
         friend std::ostream& operator<<(std::ostream &, const RMA &);
+        void RMA_PN_substitude();
+        void RMA_qty_calc();
 private:
         std::map<std::string, std::vector<Info> > m_info;
+        void proc_loc(Info &, const std::string &, int);
 };
