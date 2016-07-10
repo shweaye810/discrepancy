a336d1effad9c2fa9f673e72cb43a0ab171f985c
Shwe Aye <shweaye810@gmail.com>
2016-07-09 22:38:47 -0700

    declare is_done(), calc_qty(), extra_qty()

--- info.h
+++ info.h
@@ -6,12 +6,20 @@
 #ifndef INFO_H
 #define INFO_H
 
+
+/*
+ * @map<string, int> m_sub: string holds pn, int holds sz
+ *
+ */
 class Info {
 public:
         Info(std::string part_n, std::string model);
         Info(const Info &o);
         friend std::ostream& operator<<(std::ostream &, const Info &);
         friend class RMA;
+        bool is_done();
+        void calc_qty();
+        int extra_qty() const;
 private:
         std::string m_pn, m_mdl;
         int m_elm, m_rtv, xtr_qty, m_sub_tot;
