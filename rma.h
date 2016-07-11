9c877094314aafb69b76fa97c365eaa224e8671b
Shwe Aye <shweaye810@gmail.com>
2016-07-10 23:20:34 -0700

    declare substitude_HDD, and add preprocessor directives

--- rma.h
+++ rma.h
@@ -3,6 +3,9 @@
 #include "info.h"
 #include "tokenizer.h"
 
+#ifndef RMA_H
+#define RMA_H
+
 class RMA{
 public:
         RMA();
@@ -14,6 +17,8 @@ public:
 private:
         std::map<std::string, std::vector<Info> > m_info;
         void proc_loc(Info &, const std::string &, int);
-        void full_PN_substitude();
-
+        void substitude_full_PN();
+        void substitude_HDD();
 };
+
+#endif
