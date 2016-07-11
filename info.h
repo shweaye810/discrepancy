592a05589a9ea2cacfe7778d3e91e42627b39b66
Shwe Aye <shweaye810@gmail.com>
2016-07-10 23:15:43 -0700

    declare is_HDD(), is_MB(), is_battery(), is_adapter(), has_extra(), substitude_PN(), model()

--- info.h
+++ info.h
@@ -6,7 +6,6 @@
 #ifndef INFO_H
 #define INFO_H
 
-
 /*
  * @map<string, int> m_sub: string holds pn, int holds sz
  *
@@ -20,10 +19,18 @@ public:
         bool is_done();
         void calc_qty();
         int extra_qty() const;
+        void substitude_PN(Info &o);
+        const std::string &model() const;
+        bool has_extra();
 private:
         std::string m_pn, m_mdl;
         int m_elm, m_rtv, xtr_qty, m_sub_tot;
         std::map<std::string, int> m_sub;
+        bool is_HDD() const;
+        bool is_MB() const;
+        bool is_battery() const;
+        bool is_adapter() const;
+
 };
 
 #endif
