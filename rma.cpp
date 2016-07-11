5196451eae4eeefe326f1b1932d7200ee39cf2e5
Shwe Aye <shweaye810@gmail.com>
2016-07-10 23:19:38 -0700

    remove codes that are commented. get_qty is now part of Info. assigning QTY, PN from substitude_full_PN() has been moved into a function. implements substitude_HDD(), get_HDD().

--- rma.cpp
+++ rma.cpp
@@ -1,7 +1,9 @@
 #include <cstdlib>
 #include <exception>
+#include <vector>
 
 #include "rma.h"
+#include "product.h"
 
 using namespace std;
 
@@ -25,32 +27,30 @@ inline void RMA::proc_loc(Info &nf, const string &tmp, int qty)
         }
 }
 
-inline int get_qty(const Info &l, const Info &r)
-{
-        return abs(l.extra_qty()) < r.extra_qty() ?
-                                    abs(l.extra_qty()) : r.extra_qty();
-}
-void RMA::full_PN_substitude()
+void RMA::substitude_full_PN()
 {
         for (auto i = m_info.begin(); i != m_info.end(); i++) {
                 for (auto j = i->second.begin(); j != i->second.end(); j++) {
-                        if (j->is_done() || j->xtr_qty >= 0) {
+                        if (j->is_done() || j->xtr_qty >= 0)
                                 continue;
-                        }
-                        for (auto k = i->second.begin(); k != i->second.end(); k++) {
+                        for (auto k = i->second.begin(); k != i->second.end();
+                             k++) {
                                 if (k->is_done()) {
                                         continue;
                                 }
                                 if ((j != k && k->xtr_qty > 0) &&
                                     (j->m_mdl.find(k->m_mdl) != string::npos ||
                                      k->m_mdl.find(j->m_mdl) != string::npos)) {
-                                        //int qty = abs(j->xtr_qty) < k->xtr_qty ? abs(j->xtr_qty) : k->xtr_qty;
+                                        j->substitude_PN(*k);
+                                        /*
+                                         * replace by substidude_PN(Info &)
                                         int qty = get_qty(*j, *k);
                                         j->m_sub[k->m_pn] = qty;
                                         j->m_sub_tot += qty;
                                         k->m_sub_tot -= qty;
                                         j->calc_qty();
                                         k->calc_qty();
+                                        */
                                         if (j->is_done())
                                                 break;
                                 }
@@ -59,9 +59,54 @@ void RMA::full_PN_substitude()
         }
 }
 
+template<typename T>
+string get_hdd(const Info &o, const T &tmp)
+{
+        for (auto &i : tmp) {
+                if (o.model().find(i) != string::npos)
+                        return i;
+        }
+        return "";
+}
+
+void RMA::substitude_HDD()
+{
+        for (auto i = m_info.begin(); i != m_info.end(); i++) {
+                for (auto j = i->second.begin(); j != i->second.end(); j++) {
+                        if (j->is_done() || !j->is_HDD()|| j->has_extra())
+                                continue;
+                        for (auto k = i->second.begin(); k != i->second.end();
+                             k++) {
+                                if (k->is_done())
+                                        continue;
+                                if (j != k && k->has_extra() && k->is_HDD()) {
+                                        using prod = Product;
+                                        string j_sz = get_hdd(*j, prod::HDD_sz);
+                                        string k_sz = get_hdd(*k, prod::HDD_sz);
+                                        string j_sp = get_hdd(*j, prod::HDD_sp);
+                                        string k_sp = get_hdd(*k, prod::HDD_sp);
+                                        string j_cp = get_hdd(*j, prod::HDD_cp);
+                                        string k_cp = get_hdd(*k, prod::HDD_cp);
+                                        if (((j_sz.empty() || k_sz.empty()) ||
+                                             (j_sz == k_sz)) &&
+                                            ((j_sp.empty() || k_sp.empty()) ||
+                                             (j_sp == k_sp)) &&
+                                            ((!j_cp.empty() && !k_cp.empty()) ||
+                                             (j_cp == k_cp))) {
+                                                j->substitude_PN(*k);
+                                                if (j->is_done())
+                                                        break;
+                                        }
+                                }
+                        }
+                }
+        }
+}
+
 void RMA::substitude_PN()
 {
-        full_PN_substitude();
+        substitude_full_PN();
+        substitude_HDD();
 }
 /*
  *
@@ -78,14 +123,6 @@ void RMA::add()
         string tmp(lower(loc));
         Info nf(pn, mdl);
         proc_loc(nf, tmp, qty);
-        /*
-         * replaced by proc_loc
-         if (tmp == "rtv") {
-         nf.m_rtv = qty;
-         } else if (tmp == "elm") {
-         nf.m_elm = qty;
-         }
-        */
         if (m_info.find(rma_nm) == m_info.end()) {
                 m_info[rma_nm].push_back(nf);
         } else {
@@ -93,19 +130,10 @@ void RMA::add()
                 for (auto itr = vt.begin(); itr != vt.end(); ++itr) {
                         if (itr->m_pn == pn) {
                                 proc_loc(*itr, tmp, qty);
-                                /*
-                                 * replace by proc_loc()
-                                 if (tmp == "rtv") {
-                                 itr->m_rtv += qty;
-                                 } else if (tmp == "elm") {
-                                 itr->m_elm += qty;
-                                 }
-                                */
                                 return;
                         }
                 }
                 m_info[rma_nm].push_back(nf);
-                //cout << m_info.size();
         }
 }
 
