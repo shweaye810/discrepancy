d527369fb8894e919c961fb30df0a7fe78c38efa
Shwe Aye <shweaye810@gmail.com>
2016-07-17 17:53:29 -0700

    include product header file. rename is_battery() to is_BATT(). declare product(), prod_t, is_LCD(), is_DDR(), is_SSD(), set_product_type()

--- info.h
+++ info.h
@@ -3,6 +3,8 @@
 #include <vector>
 #include <map>
 
+#include "product.h"
+
 #ifndef INFO_H
 #define INFO_H
 
@@ -22,15 +24,20 @@ public:
         void substitude_PN(Info &o);
         const std::string &model() const;
         bool has_extra();
+        Product_type product() const;
 private:
         std::string m_pn, m_mdl;
         int m_elm, m_rtv, xtr_qty, m_sub_tot;
         std::map<std::string, int> m_sub;
+        Product_type prod_t;
         bool is_HDD() const;
         bool is_MB() const;
-        bool is_battery() const;
+        bool is_BATT() const;
         bool is_adapter() const;
-
+        bool is_LCD() const;
+        bool is_DDR() const;
+        bool is_SSD() const;
+        void set_product_type();
 };
 
 #endif
