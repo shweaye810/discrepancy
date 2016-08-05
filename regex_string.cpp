16149721daa05261e2eee2fec00b24905795862b
Shwe Aye <shweaye810@gmail.com>
2016-08-04 18:59:26 -0700

    change HDD_cap to storage_cap

--- regex_string.cpp
+++ regex_string.cpp
@@ -3,7 +3,7 @@
 namespace regex_string {
         std::regex HDD_len{ R"(([[:s:]][23].5))" };
         std::regex HDD_speed{ R"(([[:s:]][0-9]{4,}R))" };
-        std::regex HDD_cap{ R"(([[:s:]][0-9]{3,}G)|([0-9](\.[0-9])?T))" };
+        std::regex storage_cap{ R"(([[:s:]][0-9]{3,}G)|([0-9](\.[0-9])?T))" };
         std::regex CPU_model{ R"(([IE][0-9][-_][0-9]{3,}[A-Z]*)|)"
                         R"((\/[A-Z]+[0-9]{3,}[A-Z]*))" };
         std::regex adapter_watt{ R"(([[:s:]][0-9]+W))" };
